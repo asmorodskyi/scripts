2cd64a8db464f70040cd2722c630a65647cd0c93
Anton Smorodskyi <asmorodskyi@suse.com>
2016-12-04 20:46:02 +0100

    pull.sh detect that repo is not on master

--- pull.sh
+++ pull.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -x
 pushd /var/lib/openqa/tests/opensuse/
-git pull origin master
+if [ "$(git symbolic-ref --short -q HEAD)" = 'master' ]; then
+ git pull origin master
+ else
+   echo "/var/lib/openqa/tests/opensuse/ is not a master. Exiting"
+   popd
+   exit 1;
+ fi
 pushd /var/lib/openqa/tests/opensuse/products/sle/
-git pull origin master
+if [ "$(git symbolic-ref --short -q HEAD)" = 'master' ]; then
+ git pull origin master
+ else
+   echo "/var/lib/openqa/tests/opensuse/products/sle/ is not master. Exiting"
+fi
 popd
 popd
