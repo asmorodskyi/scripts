5749ca5f497f8649ff7544dd40bc89a70e438507
Anton Smorodskyi <asmorodskyi@suse.com>
2016-12-08 16:27:20 +0100

    Add script to start/stop worker with no cleanup

---
+++ startWorker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+if [ $# -eq 0 ]; then
+  systemctl stop openqa-worker@1.service
+  sudo -u _openqa-worker /usr/bin/perl /usr/share/openqa/script/worker --instance 1 --no-cleanup --verbose > /openqa-worker/worker.output &
+else
+  kill -9 $(pgrep -U _openqa-worker)
+  systemctl start openqa-worker@1.service
+fi
