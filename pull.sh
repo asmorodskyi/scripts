139b0fb1898c15d0b457df6fdd782180d55bbf98
Anton Smorodskyi <asmorodskyi@suse.com>
2016-12-20 11:23:39 +0100

    pull.sh can switch from clean branches 1. Add ability to switch from clean non-master branches 2. Code refactoring

--- pull.sh
+++ pull.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -x
+function smart_checkout() {
+  if [ "$(git symbolic-ref --short -q HEAD)" = 'master' ]; then
+   git pull origin master
+   elif [ -n "$(git status --porcelain)"]; then
+     echo "other branch selected, but it is clean , reverting";
+       git checkout master
+       git pull origin master
+       return $1='OK';
+   else
+     return $1='Fail';
+   fi
+}
 pushd /var/lib/openqa/tests/opensuse/
-if [ "$(git symbolic-ref --short -q HEAD)" = 'master' ]; then
- git pull origin master
- else
-   echo "/var/lib/openqa/tests/opensuse/ is not a master. Exiting"
-   popd
-   exit 1;
- fi
+smart_checkout result
+if [ result = "Fail" ]; then
+  echo "/var/lib/openqa/tests/opensuse/ is not a master. Exiting"
+  popd
+  exit 1;
+fi
+
 pushd /var/lib/openqa/tests/opensuse/products/sle/needles
-if [ "$(git symbolic-ref --short -q HEAD)" = 'master' ]; then
- git pull origin master
- else
+smart_checkout result
+if [ result = "Fail" ]; then 
    echo "/var/lib/openqa/tests/opensuse/products/sle/ is not master. Exiting"
 fi
 
