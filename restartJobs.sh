b407e1669cdd314d04114643daf2556889779cce
Anton Smorodskyi <asmorodskyi@suse.com>
2016-12-25 13:42:05 +0100

    Parametrize restartJob

--- restartJobs.sh
+++ restartJobs.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for job in $(openqa-client --json --host openqa.suse.de jobs result=failed arch=ppc64le distri=sle version=12-SP3 build=0187 groupid=65 | jq '.jobs | .[] | .id'); do openqa-client --host openqa.suse.de jobs/$job/restart post ; done
+HOST="openqa.suse.de"
+ARCH="ppc64le"
+DISTRI="sle"
+VERSION="12-SP3"
+BUILD="0187"
+GROUPID="65"
+for job in $(openqa-client --json --host $HOST jobs result=failed arch=$ARCH distri=$DISTRI version=$VERSION build=$BUILD groupid=$GROUPID | jq '.jobs | .[] | .id'); do openqa-client --host $HOST jobs/$job/restart post ; done
