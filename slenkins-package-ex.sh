5638b3ff14735115948f1c2310302a862d9e2d0a
Anton Smorodskyi <asmorodskyi@suse.com>
2017-01-30 11:26:51 +0100

    Fix errors

--- slenkins-package-ex.sh
+++ slenkins-package-ex.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -e
-./slenkins_package.sh
-isc build SLE_12_SP1 --no-verify --no-init; isc build SLE_12_SP2 --no-verify --no-init
+/scripts/slenkins-package.sh
+name='tests-hpc'
+destination_repository='/home/asmorodskyi/source/slenkins_package'
+cd "$destination_repository/$name"
+osc -A ibs build SLE_12_SP1 --no-verify --no-init; osc -A ibs build SLE_12_SP2 --no-verify --no-init
 slenkins-vms.sh -i client=SLE_12_SP2_openQA-x86_64-minimal_with_sdk_installed -i server=SLE_12_SP2_openQA-x86_64-minimal_with_sdk_installed -l tests-hpc
