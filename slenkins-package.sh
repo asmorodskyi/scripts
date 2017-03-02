40e5675172fd48bdd0ab317bd3c00685fc41256b
Anton Smorodskyi <asmorodskyi@suse.com>
2017-03-02 16:30:14 +0100

    Path change in one of the scripts

--- slenkins-package.sh
+++ slenkins-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 current_dir=$(pwd)
-destination_repository='/home/asmorodskyi/source/slenkins_package'
+destination_repository='/home/asmorodskyi/source/hpc_package'
 cd "$destination_repository"
 osc -A ibs build SLE_12_SP1 --no-verify --no-init; osc -A ibs build SLE_12_SP2 --no-verify --no-init
 slenkins-vms.sh -i client=SLE_12_SP2_openQA-x86_64-minimal_with_sdk_installed -i server=SLE_12_SP2_openQA-x86_64-minimal_with_sdk_installed -l tests-hpc
