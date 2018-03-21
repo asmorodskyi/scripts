00cbf33be990a66c3cd097f9d5717ce21b2e7c24
Anton Smorodskyi <asmorodskyi@suse.com>
2018-03-21 15:59:03 +0100

    Rename testsuite into alias and add execution confirmation

--- isos_post.py
+++ isos_post.py
@@ -13,7 +13,7 @@ parser.add_argument('--arch')
 parser.add_argument('--iso')
 parser.add_argument('--build')
 parser.add_argument('--test')
-parser.add_argument('--testsuite')
+parser.add_argument('--alias')
 parser.add_argument('--dryrun', action='store_true')
 args = parser.parse_args()
 allargs = '/usr/bin/openqa-client isos post _NOOBSOLETEBUILD=1 '
@@ -48,11 +48,11 @@ else:
         distri, version, flavor, arch, build)
 
 if args.test:
-    if args.testsuite:
-        sys.exit("You can't use test and testsuite at the same time")
+    if args.alias:
+        sys.exit("You can't use test and alias at the same time")
     allargs += ' TEST=' + args.test
 
-if args.testsuite:
+if args.alias:
     available_testsuites = {'mrsh': 'hpc_mrsh_master,hpc_mrsh_slave,hpc_mrsh_supportserver',
                             'munge': 'hpc_munge_master,hpc_munge_slave,hpc_munge_supportserver',
                             'pdsh': 'hpc_pdsh_master,hpc_pdsh_slave,hpc_pdsh_supportserver',
@@ -60,7 +60,7 @@ if args.testsuite:
                             'ganglia': 'hpc_ganglia_server,hpc_ganglia_client,hpc_ganglia_supportserver',
                             'pdsh_genders': 'hpc_pdsh_genders_master,hpc_pdsh_genders_slave,hpc_pdsh_genders_supportserver'}
     testsuites = []
-    testsuites = args.testsuite.split(',')
+    testsuites = args.alias.split(',')
     result_string = ''
     for item in testsuites:
         if item in available_testsuites.keys():
@@ -74,5 +74,10 @@ allargs += ' BUILD={0} DISTRI={1} VERSION={2} FLAVOR={3} ARCH={4} ISO={5}'.forma
 
 
 print 'Command to execute: \n' + allargs
+answer = ""
+while answer not in ["y", "n"]:
+    answer = raw_input("Execute [Y/N]? ").lower()
+if answer == 'n':
+    sys.exit()
 if not args.dryrun:
     print subprocess.check_output(allargs, shell=True)
