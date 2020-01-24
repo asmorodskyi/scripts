73aa9ec621e33bc92fd9b89954ff064418c91644
Anton Smorodskyi <asmorodskyi@suse.com>
2020-01-24 17:09:13 +0100

    Add hub.py

---
+++ git/hub.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+from git import Repo
+import os
+
+sys.path.append('/scripts/')
+from myutils import TaskHelper
+
+
+class GitCreatePR(TaskHelper):
+
+    def run(self, params_dict):
+        try:
+            repo = Repo(os.getcwd())
+            remote = None
+            try:
+                if repo.remotes.asmorodskyi.exists():
+                    remote = repo.remotes.asmorodskyi
+            except Exception:
+                remote = repo.remotes.origin
+            info = remote.push(repo.active_branch)[0]
+            self.logger.info(info.summary)
+            self.shell_exec(
+                "hub pull-request -m '{}'".format(repo.head.commit.message), log=True)
+        except Exception as e:
+            self.handle_error()
+
+
+def main():
+    solver = GitCreatePR('gitcreatepr', log_to_file=False)
+    solver.run({})
+
+
+if __name__ == "__main__":
+    main()
--- git/hub.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh -e
-hub pull-request -m "$(git log -1 --pretty=%B)"
