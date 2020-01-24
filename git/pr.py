0a0aa64d4a77bfe45eccc4699916718fdcb06a83
Anton Smorodskyi <asmorodskyi@suse.com>
2020-01-24 17:09:13 +0100

    Add pr.py

--- git/pr.py
+++ git/pr.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
+import sys
+from git import Repo
+import os
 
+sys.path.append('/scripts/')
 from myutils import TaskHelper
 
-class GitPull(TaskHelper):
+
+#target="${target:- asmorodskyi}"
+# git push $target && git show --no-patch --format=%B | hub pull-request -F -
+
+
+class GitCreatePR(TaskHelper):
 
     def run(self, params_dict):
-        try
-        
+        try:
+            repo = Repo(os.getcwd())
+            remote = None
+            try:
+                if repo.remotes.asmorodskyi.exists():
+                    remote = repo.remotes.asmorodskyi
+            except Exception:
+                remote = repo.remotes.origin
+            info = remote.push(repo.active_branch)[0]
+            self.logger.info(info.summary)
+            self.shell_exec(
+                "hub pull-request -m {}".format(repo.head.commit.message))
+        except Exception as e:
+            self.handle_error()
 
 
 def main():
-    solver = IsosPost('isospost')
+    solver = GitCreatePR('gitcreatepr', log_to_file=False)
     solver.run({})
 
 
