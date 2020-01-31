7c83f9daf916931ec12c105872eddcc8acd85aba
Anton Smorodskyi <asmorodskyi@suse.com>
2020-01-31 08:24:36 +0100

    clone_job: Fix regressions after refactoring

--- clone_job.py
+++ clone_job.py
@@ -11,19 +11,19 @@ class SmartClone(TaskHelper):
             parser = argparse.ArgumentParser()
             parser.add_argument('--frm', default='openqa.suse.de')
             parser.add_argument('--params')
-            parser.add_argument('--tolocal', action='store_true')
+            parser.add_argument('--winst', action='store_true')
             parser.add_argument('--jobid', required=True)
             parser.add_argument('--branch')
             parser.add_argument('--github-user', default='asmorodskyi')
             args = parser.parse_args()
             cmd = '/usr/share/openqa/script/clone_job.pl --skip-chained-deps '
-            if args.tolocal:
-                cmd += ' --within-instance'
+            if args.winst:
+                cmd += ' --within-instance {}'.format(args.frm)
             else:
                 cmd += ' --from {}'.format(args.frm)
             cmd += ' {}'.format(args.jobid)
             if args.params:
-                cmd += args.params.replace(',', ' ')
+                cmd += ' {}'.format(args.params.replace(',', ' '))
             if args.branch:
                 cmd += ' CASEDIR=https://github.com/{0}/os-autoinst-distri-opensuse.git#{1}'.format(
                     args.github_user, args.branch)
