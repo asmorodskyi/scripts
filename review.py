4a296ffbd4411e677e3fa8c2502716a14cf15981
asmorodskyi <asmorodskyi@suse.com>
2021-02-09 22:25:04 +0100

    review: apply_known feature

--- review.py
+++ review.py
@@ -58,11 +58,12 @@ class Job:
 class Review(TaskHelper):
     known_json = '/scripts/known.json'
 
-    def __init__(self, groupid: int, dry_run: bool = False):
+    def __init__(self, groupid: int, dry_run: bool = False, apply_known: bool = False):
         super(Review, self).__init__('review', log_to_file=False)
         self.cached_jobs = {}
         self.dry_run = dry_run
         self.groupid = groupid
+        self.apply_known = apply_known
         self.latest_build = self.get_latest_build(self.groupid)
         self.previous_builds = self.get_previous_builds(groupid)
         self.logger.info(self.latest_build + ' is latest build')
@@ -88,13 +89,13 @@ class Review(TaskHelper):
             pickle.dump(self.cached_jobs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def run(self):
-        # self.apply_known()
         self.refresh_cache()
         jobs_needs_review = []
         for job in self.cached_jobs.values():
             if job.needs_review(self.latest_build):
                 jobs_needs_review.append(job.id)
-
+        if self.apply_known:
+            self.apply_known_refs(jobs_needs_review)
         for need_review_id in jobs_needs_review:
             need_review_bugrefs = self.get_bugrefs(need_review_id)
             if len(need_review_bugrefs) == 0:
@@ -103,20 +104,12 @@ class Review(TaskHelper):
                 for id in previous_jobs:
                     bugrefs = bugrefs | self.get_bugrefs(id)
                 if len(bugrefs) == 0:
-                    self.logger.info('{}t{} [{}, {}, {}]'.format(self.OPENQA_URL_BASE, need_review_id,
-                                                                                self.cached_jobs[need_review_id].name,
-                                                                                self.cached_jobs[need_review_id].flavor,
-                                                                                self.cached_jobs[
-                                                                                    need_review_id].failed_modules))
-                for ref in bugrefs:
+                    job = self.cached_jobs[need_review_id]
                     self.logger.info(
-                        'Add a comment to {} with reference {}'.format(self.cached_jobs[need_review_id], ref))
-                    cmd = 'openqa-cli api --host {} -X POST jobs/{}/comments text=\'{}\''.format(self.OPENQA_URL_BASE,
-                                                                                                 need_review_id, ref)
-                    if self.dry_run:
-                        self.logger.info('"{}" not executed due to dry_run=True'.format(cmd))
-                    else:
-                        self.shell_exec(cmd, log=True)
+                        '{}t{} [{}, {}, {}]'.format(self.OPENQA_URL_BASE, need_review_id, job.name, job.flavor,
+                                                    job.failed_modules))
+                for ref in bugrefs:
+                    self.add_comment(need_review_id, ref)
 
     def get_bugrefs(self, job_id):
         bugrefs = set()
@@ -125,6 +118,15 @@ class Review(TaskHelper):
             for bug in comment['bugrefs']:
                 bugrefs.add(bug)
         return bugrefs
+
+    def add_comment(self, jobid, comment):
+        self.logger.info('Add a comment to {} with reference {}'.format(self.cached_jobs[jobid], comment))
+        cmd = 'openqa-cli api --host {} -X POST jobs/{}/comments text=\'{}\''.format(self.OPENQA_URL_BASE, jobid,
+                                                                                     comment)
+        if self.dry_run:
+            self.logger.info('"{}" not executed due to dry_run=True'.format(cmd))
+        else:
+            self.shell_exec(cmd, log=True)
 
     def get_previous_jobs(self, job_id):
         previous_jobs = []
@@ -137,24 +139,27 @@ class Review(TaskHelper):
                     previous_jobs.append(id)
         return previous_jobs
 
-    def apply_known(self):
+    def apply_known_refs(self, needs_review_ids):
         with open(Review.known_json) as f:
             known = json.load(f)
-            for item in known:
-                self.logger.info(
-                    'test={},failed_modules={},label={}'.format(item['test'], item['failed_modules'], item['label']))
+            for jobid in needs_review_ids:
+                job = self.cached_jobs[jobid]
+                for item in known:
+                    if item['test'] == job.name and item['failed_modules'] == job.failed_modules:
+                        self.add_comment(jobid, item['label'])
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--groupid', required=True)
     parser.add_argument('--dry_run', action='store_true')
+    parser.add_argument('--apply_known', action='store_true')
     args = parser.parse_args()
 
     groupids = str(args.groupid).split(',')
 
     for groupid in groupids:
-        review = Review(int(groupid), args.dry_run)
+        review = Review(int(groupid), args.dry_run, args.apply_known)
         review.run()
 
 
