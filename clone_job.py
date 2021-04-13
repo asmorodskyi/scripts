c2e8a64c14cdcdffcd32cd6c651d53e2be58ec49
asmorodskyi <asmorodskyi@suse.com>
2021-04-13 09:54:33 +0200

    clonejob: switch to openQAHelper as parent

--- clone_job.py
+++ clone_job.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
 
-from myutils import TaskHelper
+from myutils import openQAHelper
 import argparse
 
 
-class SmartClone(TaskHelper):
+class SmartClone(openQAHelper):
 
-    def generate_params_string(self, params, reset_worker, branch, github_user):
+    def __init__(self, args):
+        super(SmartClone, self).__init__('SmartClone', False, load_cache=args.loadcache)
+        self.cmd = '/usr/share/openqa/script/clone_job.pl --skip-chained-deps --parental-inheritance'
         self.params_str = ''
-        if params:
-            self.params_str = ' {}'.format(params.replace(',', ' '))
-        if reset_worker:
+        if args.params:
+            self.params_str = ' {}'.format(args.params.replace(',', ' '))
+        if args.resetworker:
             self.params_str += ' WORKER_CLASS=qemu_x86_64 '
-        if branch:
+        if args.branch:
             self.params_str += ' CASEDIR=https://github.com/{0}/os-autoinst-distri-opensuse.git#{1}'.format(
-                github_user, branch)
+                args.github_user, args.branch)
+        if args.winst:
+            self.cmd += ' --within-instance {}'.format(args.frm)
+        else:
+            self.cmd += ' --from {}'.format(args.frm)
 
-    def run(self):
+    def run(self, jobid):
         try:
-            parser = argparse.ArgumentParser()
-            parser.add_argument('--frm', default='openqa.suse.de')
-            parser.add_argument('--params')
-            parser.add_argument('--winst', action='store_true')
-            parser.add_argument('--jobid', required=True)
-            parser.add_argument('--branch')
-            parser.add_argument('--resetworker', action='store_true')
-            parser.add_argument('--github-user', default='asmorodskyi')
-            args = parser.parse_args()
-            self.generate_params_string(
-                args.params, args.resetworker, args.branch, args.github_user)
-            cmd = '/usr/share/openqa/script/clone_job.pl --skip-chained-deps --parental-inheritance'
-            if args.winst:
-                cmd += ' --within-instance {}'.format(args.frm)
-            else:
-                cmd += ' --from {}'.format(args.frm)
-            ids = args.jobid.split(',')
+            ids = jobid.split(',')
             for one_id in ids:
                 self.shell_exec('{} {} {}'.format(
-                    cmd, one_id, self.params_str), log=True)
+                    self.cmd, one_id, self.params_str), log=True)
         except Exception:
             self.handle_error()
 
 
 def main():
-    solver = SmartClone('smartclone')
-    solver.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--frm', default='openqa.suse.de')
+    parser.add_argument('--params')
+    parser.add_argument('--winst', action='store_true')
+    parser.add_argument('--jobid', required=True)
+    parser.add_argument('--branch')
+    parser.add_argument('--resetworker', action='store_true')
+    parser.add_argument('--loadcache', action='store_true', default=False)
+    parser.add_argument('--github-user', default='asmorodskyi')
+    args = parser.parse_args()
+    solver = SmartClone(args)
+    solver.run(args.jobid)
 
 
 if __name__ == "__main__":
