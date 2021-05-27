532fd51c2bba6b318dad55881b58f2127a03fcbb
asmorodskyi <asmorodskyi@suse.com>
2021-05-27 10:18:43 +0200

    delete jobs in certain group - improved version

--- openqa_delete_job.sh
+++ openqa_delete_job.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-curl 'https://openqa.suse.de/api/v1/jobs/overview?distri=sle&version=15-SP3&build=False&groupid=370' | jq -r '.[] | "openqa-client --host https://openqa.suse.de jobs/\(.id) delete # \(.name)"'
+GROUPID=${1?"openQA group id needed"}
+VERSION=${2?"SLE version needed"}
+BUILD=${3?"build needed"}
+DRY_RUN=${4?"dry run (0 - no everything else yes)"}
+IDS=$(curl "https://openqa.suse.de/api/v1/jobs/overview?distri=sle&version=$VERSION&build=$BUILD&groupid=$GROUPID" | jq '.[] | .id')
+IDS_ARRAY=($IDS)
+for i in "${IDS_ARRAY[@]}"
+do
+  if [[ $DRY_RUN -eq 0 ]] ; then
+    echo "DELETING JOB: $i"
+    /usr/bin/openqa-client jobs/$i delete --host https://openqa.suse.de
+  else
+    echo "JOB TO DELETE:"
+	  /usr/bin/openqa-client --json-output jobs/$i get --host https://openqa.suse.de | jq -jr '.job |"ID: ", .id," ","JOB NAME: ", .name," ","TEST SUITE: ", .test, "\n"'
+  fi
+done
