9a36814a069ca1d3b127b927968283be8a2939ae
asmorodskyi <asmorodskyi@suse.com>
2021-07-20 11:44:29 +0200

    terraform: switch to containerized version of terraform

--- pc/terraform.sh
+++ pc/terraform.sh
@@ -2,19 +2,22 @@
 CSP=${1:azure}
 IMAGE=$2
 TERRAFOR_FOLDER="/tmp/$CSP"
+TERRAFORM_VERSION=0.13.4
+PODMAN_EXE="podman run -v $TERRAFOR_FOLDER:/tmp/ --env-host=true -w=/tmp  -v /home/asmorodskyi/:/home/asmorodskyi hashicorp/terraform:0.13.4"
+COMMON_PARAMS='-var tags={"openqa_ttl":"8640300"} -auto-approve'
 if [ -d $TERRAFOR_FOLDER ]; then
   rm $TERRAFOR_FOLDER/*.tf
   rm -rf $TERRAFOR_FOLDER/.terraform
+else
+  mkdir -p $TERRAFOR_FOLDER
 fi
-mkdir -p $TERRAFOR_FOLDER
 cp /scripts/pc/terraform/$CSP.tf $TERRAFOR_FOLDER
 cd $TERRAFOR_FOLDER
-terraform init
+$PODMAN_EXE init
 if [ $CSP = "gce" ]; then
-  terraform plan -var "image_id=$IMAGE" -var 'instance_count=1' -var 'type=n1-standard-2' -var 'region=europe-west1-b' -var 'name=openqa-suse-de' -var 'project=suse-sle-qa' -var 'tags={"openqa_ttl":"8640300"}' -out myplan
+  $PODMAN_EXE apply -var "image_id=$IMAGE" -var 'instance_count=1' -var 'type=n1-standard-2' -var 'region=europe-west1-b' -var 'name=openqa-suse-de' -var 'project=suse-sle-qa' $COMMON_PARAMS
 elif [ $CSP = "azure" ]; then
-  terraform plan -var 'name=openqa-suse-de' -var 'tags={"openqa_ttl":"7500"}' -var 'offer=sles-15-sp2-byos' -var 'type=Standard_B1ms' -out myplan
+  $PODMAN_EXE apply -var 'name=openqa-suse-de' -var 'offer=sles-15-sp2-byos' -var 'type=Standard_B1ms' $COMMON_PARAMS
 elif [ $CSP = "ec2" ]; then
-  terraform plan -no-color -var "image_id=$IMAGE" -var 'instance_count=1' -var 'type=t2.large' -var 'region=eu-west-1' -var 'name=openqa-suse-de' -var 'tags={"openqa_ttl":"8640300"}' -out myplan
+  $PODMAN_EXE apply -var "image_id=$IMAGE" -var 'instance_count=1' -var 'type=t2.large' -var 'region=eu-west-1' -var 'name=openqa-suse-de'  $COMMON_PARAMS
 fi
-terraform apply "myplan"
