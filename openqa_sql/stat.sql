db5d4168dfba9290271bafb46042c1607d168576
asmorodskyi <asmorodskyi@suse.com>
2021-11-15 20:28:06 +0100

    sql : add query to track all job groups needed for review

--- openqa_sql/stat.sql
+++ openqa_sql/stat.sql
@@ -1,2 +1,3 @@
 select age(t_finished,t_started) from jobs where t_created > '2016-01-04'::date and t_started is not null and t_finished is not null;
 select avg(t_finished-t_started) from jobs where t_created > '2016-01-04'::date and t_started is not null and t_finished is not null
+select id from job_groups where parent_id in (27, 35, 313, 32, 33);
