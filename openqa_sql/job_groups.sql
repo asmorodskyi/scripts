23f9e9ff0488bf6cf65ac47592e45c9460643fc9
asmorodskyi <asmorodskyi@suse.com>
2021-12-23 18:26:31 +0100

    sql: add query to get full list of all job groups which we are responsible for

--- openqa_sql/job_groups.sql
+++ openqa_sql/job_groups.sql
@@ -1,1 +1,2 @@
 select name from job_groups where id in (select group_id from jobs where id in (select job_id from job_modules where t_created > '2017-02-01'::date and script='tests/support_server/boot.pm') group by group_id);
+select id,name from job_groups where parent_id in (27, 35, 313, 32, 33) order by 1;
