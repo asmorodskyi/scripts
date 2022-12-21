7c855b400e556e6b9d440a3137e90c98b926fe04
mdati <mdati@suse.com>
2022-12-21 11:38:23 +0100

    Proposal of additional diffJobSettings functionality - skipdiff opt added: list of settings fields to skip from all diffs; default NAME

--- diffJobSettings.py
+++ diffJobSettings.py
@@ -7,13 +7,16 @@ import requests
 import json
 
 parser = argparse.ArgumentParser(description='openqa test data:display the  differences of Settings betwen 2 given tests IDs Or display the full data set of a single test ID')
-parser.add_argument('--host', default='openqa.suse.de',help='default openqa.suse.de')
+parser.add_argument('--host', default='openqa.suse.de',help='Hostname; Default openqa.suse.de')
 parser.add_argument('--jobids','-j', help='jobid1[,jobid2] # Enter 1[Full data] or 2[Differences] IDs')
-parser.add_argument('--nossl','-n', action='store_true', help='http urls; default https')
-parser.add_argument('--comments','-c', action='store_true', help='print job comments too')
+parser.add_argument('--nossl','-n', action='store_true', help='http without ssl; Default https')
+parser.add_argument('--comments','-c', action='store_true', help='print job comments too; Default none')
+parser.add_argument('--skipfield','-s', default='NAME', help='F1[,F2,...] list of job.setting FiledNames to skip from diff. Default NAME')
 args = parser.parse_args()
 
 source_url=''
+# Parameter list, to skip from diff.check default in argument parser
+obvious_differs=args.skipfield.split(",")
 
 if not (args.jobids):
     parser.print_help()
@@ -26,28 +29,34 @@ if args.nossl:
 else:
     source_url = 'https://'
 
-# default OSD in parser
+# default host OSD set in argument parser
 source_url += args.host + '/api/v1/jobs/'
-# Parameter list, to skip from diff.check
-obvious_differs=()
 
 print(f'Result url:{source_url} - jobids:{ids}')
 
 if len(ids) < 1:
     exit(1)
 else:
-    group_json1   = requests.get(source_url+ids[0]).json()
-    if args.comments:
-        group_json1_c = requests.get(source_url+ids[0]+'/comments').json()
+    try:
+        group_json1 = requests.get(source_url+ids[0]).json()
+        if args.comments:
+            group_json1_c = requests.get(source_url+ids[0]+'/comments').json()
+    except:
+        print(f"ID {ids[0]} data not found or wrong query,check hostname/ssl")
+        exit(1)
 
 if len(ids) == 1:
     print('\n',json.dumps(group_json1,indent=2))
     if args.comments:
         print('\n',json.dumps(group_json1_c,indent=2))
 else:
-    group_json2   = requests.get(source_url+ids[1]).json()
-    if args.comments:
-        group_json2_c = requests.get(source_url+ids[1]+'/comments').json()
+    try:
+        group_json2 = requests.get(source_url+ids[1]).json()
+        if args.comments:
+            group_json2_c = requests.get(source_url+ids[1]+'/comments').json()
+    except:
+        print(f"ID {ids[1]} data not found or wrong query,check hostname/ssl")
+        exit(1)
 
     diff_data1 = dict()
     diff_data2 = dict()
@@ -72,12 +81,18 @@ else:
 
     # job 1 only KEYS
     for key in group_json1['job']['settings'].keys():
+        if key in obvious_differs:
+            continue
+
         if key not in group_json2['job']['settings']:
             value=group_json1['job']['settings'][key]
             extra_first.update({key:value})
 
     # job 2 only KEYS
     for key in group_json2['job']['settings'].keys():
+        if key in obvious_differs:
+            continue
+
         if key not in group_json1['job']['settings']:
             value=group_json2['job']['settings'][key]
             extra_second.update({key:value})
