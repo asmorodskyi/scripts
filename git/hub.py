4fbb522aabf56e3ba7790da83a34f8752bad235c
Anton Smorodskyi <asmorodskyi@suse.de>
2023-03-16 11:37:26 +0100

    Replace hub with gh

--- git/hub.py
+++ git/hub.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import sys
-
 from myutils import GitHelper
 
 
@@ -9,8 +7,10 @@ class GitCreatePR(GitHelper):
     def run(self):
         info = self.remote.push(self.repo.active_branch)[0]
         self.logger.info(info.summary)
+        msg = self.repo.head.commit.message
+
         self.shell_exec(
-            "hub pull-request -m '{}'".format(self.repo.head.commit.message), log=True)
+            f"gh pr create --title '{msg}' --body '{msg}'", log=True)
 
 
 def main():
