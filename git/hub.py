07364ad0b4eba98e207efd656a1192541a0958f0
Anton Smorodskyi <asmorodskyi@suse.de>
2023-07-04 15:21:33 +0200

    Add ability to detect multiline commit message so ghub will create different title and body on that case

--- git/hub.py
+++ git/hub.py
@@ -8,9 +8,12 @@ class GitCreatePR(GitHelper):
         info = self.remote.push(self.repo.active_branch)[0]
         self.logger.info(info.summary)
         msg = self.repo.head.commit.message
-
-        self.shell_exec(
-            f"gh pr create --title '{msg}' --body '{msg}'", log=True)
+        msg_arr = msg.partition('\n')
+        if len(msg_arr) >1:
+            body = ''.join(msg_arr[1:])
+            self.shell_exec(f"gh pr create --title '{msg_arr[0]}' --body '{body}'", log=True)
+        else:
+            self.shell_exec(f"gh pr create --title '{msg}' --body '{msg}'", log=True)
 
 
 def main():
