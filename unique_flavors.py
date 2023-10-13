597976f51be8f69903ae52a1983678e6e8fae5f5
Anton Smorodskyi <asmorodskyi@suse.de>
2023-10-13 16:37:20 +0200

    Script collecting unique_flavors from bot-ng

---
+++ unique_flavors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+import yaml
+
+
+def main():
+    collected_flavors = set()
+    for yaml_file in Path('/home/asmorodskyi/source/metadata/bot-ng/').glob('pub*.yml'):
+        contents = yaml.safe_load(Path(yaml_file).read_text(encoding="utf-8"))
+        if 'aggregate' in contents:
+            collected_flavors.add(contents['aggregate']['FLAVOR'])
+        if 'incidents' in contents:
+            for field in contents['incidents']['FLAVOR']:
+                collected_flavors.add(field)
+    print(collected_flavors)
+
+
+if __name__ == "__main__":
+    main()
