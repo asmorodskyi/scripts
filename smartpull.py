8fdd03666a57d9c46f87f550ebf31457538a5fef
Anton Smorodskyi <asmorodskyi@suse.de>
2023-11-25 22:02:01 +0100

    If we not on clean master simply fetch instead of pull

--- smartpull.py
+++ smartpull.py
@@ -7,35 +7,25 @@ class SmartPull(TaskHelper):
 
     def run(self, repo_path):
         try:
-            self.logger.info("#### START processing repo %s", repo_path)
+            self.logger.info(f"#### START processing repo {repo_path}")
             repo = Repo(repo_path)
-            self.logger.info("Current branch is %s with commit %s",
-                             repo.active_branch, repo.commit())
-            was_dirty = False
-            if repo.is_dirty():
-                self.logger.info("HEAD is dirty, trying to stash changes")
-                repo.git.stash('save')
-                was_dirty = True
+            initial_commit = repo.commit()
             if 'master' in repo.remote().refs:
                 master_branch = 'master'
             else:
                 master_branch = 'main'
-            old_branch = master_branch
-            if str(repo.active_branch) != master_branch:
-                self.logger.info(
-                    "HEAD pointing to %s,trying switch to %s", repo.active_branch, master_branch)
-                old_branch = repo.active_branch
-                repo.git.checkout(master_branch)
-            repo.remote().pull(master_branch)
-            self.logger.info(
-                "After pulling changes head pointing to %s", repo.commit())
-            if old_branch != master_branch:
-                self.logger.info(
-                    "HEAD was at %s, switching it back", old_branch)
-                repo.git.checkout(old_branch)
-            if was_dirty:
-                self.logger.info("%s was dirty, so reapplying changes", master_branch)
-                repo.git.stash('apply')
+            if repo.is_dirty():
+                self.logger.warning("HEAD is dirty, will only fetch")
+                repo.remote().fetch()
+            elif str(repo.active_branch) != master_branch:
+                self.logger.warning(f"HEAD pointing to {repo.active_branch}, will only fetch")
+                repo.remote().fetch()
+            else:
+                self.logger.info("HEAD is clean, will pull")
+                repo.remote().pull(master_branch)
+            if initial_commit != repo.commit():
+                self.logger.warning("master branch updated")
+
         except Exception:
             self.handle_error()
         finally:
