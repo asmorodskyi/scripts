5de0c5a6682c3604dc0f6de45a0c348bcd780713
Anton Smorodskyi <asmorodskyi@suse.de>
2023-11-28 22:28:42 +0100

    Add ca-certificates-suse dep

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -xe
 #install deps for this project
-zypper in python3-logzero python3-GitPython python3-SQLAlchemy python3-psycopg2
+zypper in python3-logzero python3-GitPython python3-SQLAlchemy python3-psycopg2 ca-certificates-suse
