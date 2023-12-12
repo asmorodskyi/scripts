d47c9e1e83a19ff40884fc6cb6839bd896ab50ef
Anton Smorodskyi <asmorodskyi@suse.de>
2023-12-12 19:21:04 +0100

    Add more places to clean up

--- clean_GPUCache.sh
+++ clean_GPUCache.sh
@@ -2,3 +2,7 @@
 rm -f ~/.config/Slack/GPUCache/*
 rm -f ~/.config/chromium/Default/GPUCache/*
 rm -f ~/.config/Code/GPUCache/*
+rm -f ~/Templates/Code/GPUCache/*
+rm -f ~/.config/chromium/Profile\ 1/GPUCache/*
+rm -f ~/.config/discord/GPUCache/*
+rm -f ~/.local/share/openSUSE/org.opensuse.opensuse_welcome/QtWebEngine/Default/GPUCache/*
