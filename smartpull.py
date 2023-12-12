a5a0c2b89bc1e0e6a473ba7bbefc5caa6f181f15
Anton Smorodskyi <asmorodskyi@suse.de>
2023-12-12 19:21:31 +0100

    Tidy up logging

--- smartpull.py
+++ smartpull.py
@@ -7,7 +7,6 @@ class SmartPull(TaskHelper):
 
     def run(self, repo_path):
         try:
-            self.logger.info(f"#### START processing repo {repo_path}")
             repo = Repo(repo_path)
             initial_commit = repo.commit()
             if 'master' in repo.remote().refs:
@@ -15,21 +14,19 @@ class SmartPull(TaskHelper):
             else:
                 master_branch = 'main'
             if repo.is_dirty():
-                self.logger.warning("HEAD is dirty, will only fetch")
+                self.logger.warning(f"{repo_path} HEAD is dirty, will only fetch")
                 repo.remote().fetch()
             elif str(repo.active_branch) != master_branch:
-                self.logger.warning(f"HEAD pointing to {repo.active_branch}, will only fetch")
+                self.logger.warning(f"{repo_path} HEAD pointing to {repo.active_branch}, will only fetch")
                 repo.remote().fetch()
             else:
                 self.logger.info("HEAD is clean, will pull")
                 repo.remote().pull(master_branch)
-            if initial_commit != repo.commit():
-                self.logger.warning("master branch updated")
+                if initial_commit != repo.commit():
+                    self.logger.warning("master branch updated")
 
         except Exception:
             self.handle_error()
-        finally:
-            self.logger.info("#### End processing repo %s", repo_path)
 
 
 def main():
