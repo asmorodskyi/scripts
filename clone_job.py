0db1005541a4e2f3d8fc231cf9997e2887219875
Anton Smorodskyi <asmorodskyi@suse.de>
2023-12-14 14:40:26 +0100

    Switch to f-strings

--- clone_job.py
+++ clone_job.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-from myutils import openQAHelper
 import argparse
 import re
 import urllib3
+from myutils import openQAHelper
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,22 +15,21 @@ class SmartClone(openQAHelper):
         self.cmd = '/usr/share/openqa/script/clone_job.pl --skip-chained-deps --parental-inheritance'
         self.params_str = ''
         if args.params:
-            self.params_str = ' {}'.format(args.params.replace(',', ' '))
+            self.params_str = f' {args.params.replace(",", " ")}'
         if args.resetworker:
             self.params_str += ' WORKER_CLASS=qemu_x86_64 '
         if args.branch:
-            self.params_str += ' CASEDIR=https://github.com/{0}/os-autoinst-distri-opensuse.git#{1} BUILD={1}'.format(
-                args.github_user, args.branch)
+            self.params_str += f' CASEDIR=https://github.com/{args.github_user}/os-autoinst-distri-opensuse.git#{args.branch} BUILD={args.branch}'
         if args.winst:
-            self.cmd += ' --within-instance {}'.format(args.frm)
+            self.cmd += f' --within-instance {args.frm}'
         else:
-            self.cmd += ' --from {}'.format(args.frm)
+            self.cmd += f' --from {args.frm}'
 
     def run(self, jobid, dryrun: bool = False):
         try:
             ids = jobid.split(',')
             for one_id in ids:
-                self.shell_exec('{} {} {}'.format(self.cmd, one_id, self.params_str), log=True, dryrun=dryrun)
+                self.shell_exec(f'{self.cmd} {one_id} {self.params_str}', log=True, dryrun=dryrun)
         except Exception:
             self.handle_error()
 
@@ -42,22 +41,24 @@ class SmartClone(openQAHelper):
             unique_jobs = self.osd_get_jobs_where(
                 latest_build, groupid, " and test = 'publiccloud_download_testrepos'")
             for job in unique_jobs:
-                self.shell_exec('{} {} {}'.format(self.cmd, job.id, self.params_str), log=True, dryrun=dryrun)
+                self.shell_exec(f'{self.cmd} {job.id} {self.params_str}', log=True, dryrun=dryrun)
         elif query.startswith('fixworker'):
             if not latest_build:
-                raise ValueError("No jobs was found in {}".format(groupid))
+                raise ValueError(f"No jobs was found in {groupid}")
             unique_jobs = self.osd_get_jobs_where(
                 latest_build, groupid, " and test != 'publiccloud_download_testrepos'")
             for job in unique_jobs:
+                new_worker_class = ''
                 if job.flavor.startswith('EC2'):
-                    new_params = "{} {} ".format(self.params_str, 'WORKER_CLASS=pc_ec2')
+                    new_worker_class='pc_ec2'
                 elif job.flavor.startswith('AZURE'):
-                    new_params = "{} {}".format(self.params_str, 'WORKER_CLASS=pc_azure')
+                    new_worker_class='pc_azure'
                 elif job.flavor.startswith('GCE'):
-                    new_params = "{} {}".format(self.params_str, 'WORKER_CLASS=pc_gce')
+                    new_worker_class='pc_gce'
                 else:
-                    raise AttributeError('Unexpected job flavor {}'.format(job))
-                self.shell_exec('{} {} {}'.format(self.cmd, job.id, new_params), log=True, dryrun=dryrun)
+                    raise AttributeError(f'Unexpected job flavor {job}')
+                new_params = f"{self.params_str} WORKER_CLASS={new_worker_class}"
+                self.shell_exec(f'{self.cmd} {job.id} {new_params}', log=True, dryrun=dryrun)
         else:
             raise AttributeError('Unexpected query %s, excpected : allpc=<groupid>,fixworker=<groupid>')
 
---
+++ smartcalendar.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
