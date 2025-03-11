a47ebc5bcabb87040eaac30acb0a759cf926dc34
Anton Smorodskyi <asmorodskyi@suse.com>
2025-03-11 10:00:29 +0100

    Also delete public_cloud passwords

--- clean_pass.sh
+++ clean_pass.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -xe
 echo 'export OBS_TOKEN=ddddddddddddddddd' > /home/asmorodskyi/source/gravity-cannon/token
+rm -f /home/asmorodskyi/creds/ccoe/Azure.json
+rm -f /home/asmorodskyi/creds/ccoe/EC2.json
+rm -f /home/asmorodskyi/creds/ccoe/GCE.json
