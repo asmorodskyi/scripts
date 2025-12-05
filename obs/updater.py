5dd5b075110edfc0df7b2d0d4a387535e93f43bf
Anton Smorodskyi <anton.smorodskyi@suse.com>
2025-12-05 08:43:14 +0100

    New version of attempt to download new archive based on data from spec file

--- obs/updater.py
+++ obs/updater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import logging
 from pathlib import Path
 from osctiny import Osc
@@ -36,12 +37,28 @@ class Updater:
         Path(self.package_path).mkdir()
         self.osc_package.checkout(self.project, self.package, self.package_path)
 
+    def _get_from_spec(self, regex):
+        pattern = re.compile(regex)
+        with open(f"{self.package_path}/{self.package}.spec", 'r') as file:
+            for line in file:
+                match = pattern.match(line)
+                if match:
+                    return match.group(1).strip()
+        raise ValueError(f"{regex} was not found")
+
+    def get_new_version(self):
+        version = self._get_from_spec(r"^\s*Version:\s*(.*)")
+        source = self._get_from_spec(r"^\s*Source:\s*(.*)")
+        logger.info(source.replace('%{version}', version))
+
+
 
 
 def main():
     updater = Updater("openSUSE:Factory", "python-BTrees")
     updater.create_package_branch()
     updater.checkout_package()
+    updater.get_new_version()
 
 
 if __name__ == "__main__":
