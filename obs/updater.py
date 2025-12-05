d59649d6333d06a1401c4cec1dfb585633c2f4a1
Anton Smorodskyi <anton.smorodskyi@suse.com>
2025-12-05 08:36:21 +0100

    Remove attempt to automatically fetch archive

--- obs/updater.py
+++ obs/updater.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from osctiny import Osc
 from osctiny.extensions.packages import Package
 import configparser
-from specfile.specfile import Specfile
-from specfile.macros import Macros; Macros.dump()
 
 logger = logging.getLogger("osc")
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
@@ -38,20 +36,12 @@ class Updater:
         Path(self.package_path).mkdir()
         self.osc_package.checkout(self.project, self.package, self.package_path)
 
-    def get_new_version(self):
-        PYTHON_MODULE_MACRO = ('%python_module', 'python3-')
-
-        # 2. Add the tuple to a list for the 'macros' parameter
-        MACRO_TUPLES = [PYTHON_MODULE_MACRO]
-        specfile = Specfile(f"{self.package_path}/{self.package}.spec", macros=MACRO_TUPLES, force_parse=True)
-        logger.info(specfile.URL)
 
 
 def main():
     updater = Updater("openSUSE:Factory", "python-BTrees")
-    #updater.create_package_branch()
-    #updater.checkout_package()
-    updater.get_new_version()
+    updater.create_package_branch()
+    updater.checkout_package()
 
 
 if __name__ == "__main__":
